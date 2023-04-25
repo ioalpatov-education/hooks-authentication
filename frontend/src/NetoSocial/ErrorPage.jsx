7cd6b7af3f95cef44720213be9081928299818ef
IlyaAlpatov <alpatov30@gmail.com>
2023-04-25 12:07:12 +0300

    добавлен хеддер к странице 404

--- frontend/src/NetoSocial/ErrorPage.jsx
+++ frontend/src/NetoSocial/ErrorPage.jsx
@@ -1,8 +1,33 @@
+import UserInfo from "./components/UserInfo";
+import { useEffect, useContext } from "react";
+import { NewsContext } from "../App";
+import { useNavigate } from "react-router-dom";
+
 const ErrorPage = () => {
+  const { userProfile, setUserProfile } = useContext(NewsContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      navigate("/");
+      return;
+    }
+
+    setUserProfile(JSON.parse(localStorage.getItem("profile")));
+  }, []);
+
+  const headerRight = !!userProfile ? <UserInfo /> : null;
+
   return (
-    <div className="error-page">
-      <h1>404</h1>
-      <h2>Not Found</h2>
+    <div className="neto-social">
+      <header className="neto-social__header">
+        <h3>Neto Social</h3>
+        {headerRight}
+      </header>
+      <div className="error-page">
+        <h1>404</h1>
+        <h2>Not Found</h2>
+      </div>
     </div>
   );
 };
